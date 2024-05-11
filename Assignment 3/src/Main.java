f5e346bd07076957f055fd6e7ec069a28ce791fb
Raikhanov <145896630+Raikhanov@users.noreply.github.com>
2024-05-11 12:28:20 +0500

    Update Main.java

--- Assignment 3/src/Main.java
+++ Assignment 3/src/Main.java
@@ -1,18 +1,29 @@
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        BinarySearchTree<Integer, String> bst = new BinarySearchTree<>();
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Input 6 keys and value ENTER");
-        bst.put(sc.nextInt(), sc.nextLine());
-        bst.put(sc.nextInt(), sc.nextLine());
-        bst.put(sc.nextInt(), sc.nextLine());
-        bst.put(sc.nextInt(), sc.nextLine());
-        bst.put(sc.nextInt(), sc.nextLine());
-        bst.put(sc.nextInt(), sc.nextLine());
-        for (var node : bst) {
-            System.out.println("Key is " + node.getKey() + " and value is " + node.getValue());
+        BinaryTree tree = new BinaryTree();
+
+        // Insert some elements into the tree
+        tree.insert(50);
+        tree.insert(30);
+        tree.insert(20);
+        tree.insert(40);
+        tree.insert(70);
+        tree.insert(60);
+        tree.insert(80);
+
+        // Print the size of the tree
+        System.out.println("Size of the tree: " + tree.size());
+
+        // Print the elements of the tree using in-order traversal
+        System.out.print("In-order traversal: ");
+        tree.inOrder();
+        System.out.println();
+
+        // Print the key-value pairs using the iterator
+        System.out.println("Key-value pairs:");
+        for (Object elem : tree) {
+            BinaryTree.TreeEntry entry = (BinaryTree.TreeEntry) elem;
+            System.out.println("key is " + entry.getKey() + " and value is " + entry.getValue());
         }
     }
 }
